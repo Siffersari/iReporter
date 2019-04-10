6dab8acaa4f263865bf352789ffc931744608ad6
Leewel <leewelkarani@gmail.com>
2019-04-10 23:32:36 +0300

    [Finishes #165221666] Add  Header and Register components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import Register from "./auth/Register";
+import Header from "./layout/Header";
+
+const alertOptions = {
+  timeout: 3600,
+  position: "top center"
+};
 
 export class App extends Component {
   render() {
-    return <div>React App</div>;
+    return (
+      <AlertProvider template={AlertTemplate} {...alertOptions}>
+        <Router>
+          <Fragment>
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/register" component={Register} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </AlertProvider>
+    );
   }
 }
 
