1b2ba38a5429df2878704cefa58e0585c0d9c297
Leewel <leewelkarani@gmail.com>
2019-04-14 22:44:02 +0300

    [Start #165330439] Fetch specific redflag record

---
+++ src/components/redflags/Flag.js
@@ -0,0 +1,139 @@
+import React, { Component, Fragment } from "react";
+import { withAlert } from "react-alert";
+import axios from "axios";
+import { getTokenConfig } from "../../actions/auth";
+
+export class Flag extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flags: [],
+      isError: null,
+      isLoaded: null
+    };
+  }
+
+  fetchFlag(url) {
+    const { alert } = this.props;
+    const config = getTokenConfig();
+    axios
+      .get(url, config)
+      .then(res => {
+        this.setState({
+          flags: res.data,
+          isLoaded: true
+        });
+      })
+      .catch(err => {
+        this.setState({
+          isError: true
+        });
+        if (err.response.data.detail) {
+          if (err.response.data.detail.includes("signature")) {
+            alert.error("Please login in to continue");
+            this.props.history.push("/login");
+          }
+        } else if (err.response.data.message) {
+          alert.error(err.response.data.message);
+        } else {
+          this.props.history.push("/login");
+        }
+      });
+  }
+
+  componentDidMount() {
+    const flagId = window.location.href.split("=")[1];
+
+    this.fetchFlag(
+      `https://ireporter-drf-api-staging.herokuapp.com/api/redflags/${flagId}/`
+    );
+  }
+
+  render() {
+    if (this.state.isLoaded) {
+      const { flags } = this.state;
+
+      const redflag = flags.data;
+
+      const topMargin = {
+        marginTop: "1.5em",
+        marginBottom: "1.5em"
+      };
+
+      const noDecoration = {
+        textDecoration: "none"
+      };
+
+      return (
+        <Fragment>
+          <div className="container-fluid col-9">
+            <div className="card rounded" style={topMargin} key={redflag.id}>
+              <object
+                data="https://picsum.photos/200/300/?random"
+                type="image/png"
+              >
+                <img
+                  src={[
+                    "https://ireporter-drf-api-staging.herokuapp.com" +
+                      redflag.Image
+                  ]}
+                  alt=""
+                  className="card-img-top"
+                />
+              </object>
+
+              <div className="card-body">
+                <h3 className="card-title">
+                  {" "}
+                  <a style={noDecoration}>
+                    <strong>{redflag.title}</strong>
+                  </a>
+                </h3>
+                <span>
+                  {" "}
+                  Created By: <p className="text-muted">{redflag.createdBy}</p>
+                </span>{" "}
+                <span>
+                  {" "}
+                  Created On: <p className="text-muted">{redflag.createdOn}</p>
+                </span>{" "}
+                <span>
+                  {" "}
+                  Location: <p className="text-muted">{redflag.location}</p>
+                </span>{" "}
+                <span>
+                  {" "}
+                  Status: <p className="badge ">{redflag.status}</p>
+                </span>{" "}
+                <p className="card-text">{redflag.comment}</p>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      );
+    } else if (this.state.isError) {
+      return (
+        <div className="d-flex justify-content-center mt-5">
+          <h2 className="text-center mt-5">
+            SORRY, THIS REQUEST COULD NOT BE COMPLETED
+          </h2>
+        </div>
+      );
+    } else {
+      const divStyle = {
+        width: "10rem",
+        height: "10rem",
+        marginTop: "18em"
+      };
+      return (
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border" style={divStyle} role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+export default withAlert()(Flag);
