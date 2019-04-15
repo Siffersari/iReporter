cafb0a59b534f1ea4b07c13bb5893f87a37d5df5
Leewel <leewelkarani@gmail.com>
2019-04-15 17:08:18 +0300

    [Start #165343869] Add fetch and authentication

--- src/actions/redflag.js
+++ src/actions/redflag.js
@@ -25,11 +25,52 @@ export const postFlag = (flag, currentProps) => {
         alert.error(err.response.data.non_field_errs.join());
       if (err.response.data.location)
         alert.error(`Location: ${err.response.data.location.join()}`);
-      if (err.response.data.detail.includes("signature")) {
-        alert.error("Please login in to continue");
-        currentProps.history.push("/login");
-      } else {
-        currentProps.history.push("/login");
+      if (err.response.data.detail) {
+        if (err.response.data.detail.includes("signature")) {
+          alert.error("Please login in to continue");
+          this.props.history.push("/login");
+        }
+      } else if (err.response.data.error) {
+        alert.error(err.response.data.error);
+      }
+    });
+};
+
+export const editFlag = (flag, id, currentProps) => {
+  const { alert } = currentProps;
+
+  const config = getTokenConfig();
+
+  axios
+    .put(
+      `https://ireporter-drf-api-staging.herokuapp.com/api/redflags/${id}/`,
+      flag,
+      config
+    )
+    .then(res => {
+      alert.success("Redflag record updated successfully");
+      currentProps.history.push(`/flag/?fid=${id}`);
+    })
+    .catch(err => {
+      if (err.response.data) {
+        if (err.response.data.message) alert.error(err.response.data.message);
+        if (err.response.data.title)
+          alert.error(`Title: ${err.response.data.title.join()}`);
+        if (err.response.data.comment)
+          alert.error(`Comment: ${err.response.data.comment.join()}`);
+        if (err.response.data.non_field_errs)
+          alert.error(err.response.data.non_field_errs.join());
+        if (err.response.data.location)
+          alert.error(`Location: ${err.response.data.location.join()}`);
+      }
+
+      if (err.response.data.detail) {
+        if (err.response.data.detail.includes("signature")) {
+          alert.error("Please login in to continue");
+          this.props.history.push("/login");
+        }
+      } else if (err.response.data.error) {
+        alert.error(err.response.data.error);
       }
     });
 };
